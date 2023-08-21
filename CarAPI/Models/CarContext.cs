58844e4e2c4e3d20e0df24c020eeb9429fd14c65
Lucas <lucas@plannerti.com.br>
2023-08-21 14:45:43 -0300

    Iniciando o estudo criando uma parte da API

---
+++ CarAPI/Models/CarContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarAPI.Models
+{
+    public class CarContext : DbContext 
+    {
+        public CarContext(DbContextOptions<CarContext> options) : base(options)
+        {
+        }
+        public DbSet<Cars> Cars { get; set; } = null!;
+    }
+    
+}
---
+++ CarAPI/Models/CarDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CarAPI.Models
+{
+    public class CarDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public int YearManufacture { get; set; }
+        public string? Color { get; set; }
+    }
+}
