56a9cce4b3858d22224e7c4f62059821f07fbb49
Lucas <lucas@plannerti.com.br>
2023-08-23 11:26:12 -0300

    Criando o metodo post/criar da controller

--- CarAPI/Controllers/CarController.cs
+++ CarAPI/Controllers/CarController.cs
@@ -17,20 +17,41 @@ namespace CarAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CarDTO>>> GetCar()
+        public async Task<ActionResult<IEnumerable<CarDTO>>> GetCars()
         {
             return await _context.Cars
                 .Select(x => TodoCarDTO(x))
                 .ToListAsync();
         }
 
-        private static CarDTO TodoCarDTO(Cars cars) => new CarDTO
+        [HttpPost]
+        public async Task<ActionResult<CarDTO>> PostCars(CarDTO carsDTO)
         {
-            Id = cars.Id,
-            Name = cars.Name,
-            Brand = cars.Brand,
-            YearManufacture = cars.YearManufacture,
-            Color = cars.Color
-        };
+            var cars = new Cars
+            {
+                Name = carsDTO.Name,
+                Brand = carsDTO.Brand,
+                Color = carsDTO.Color,
+                YearManufacture = carsDTO.YearManufacture
+            };
+
+            _context.Cars.Add(cars);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCars), new { id = cars.Id }, TodoCarDTO(cars));
+            
+        }
+
+
+        private static CarDTO TodoCarDTO(Cars cars)
+            => new CarDTO
+            {
+
+                Id = cars.Id,
+                Name = cars.Name,
+                Brand = cars.Brand,
+                YearManufacture = cars.YearManufacture,
+                Color = cars.Color
+            };
     }
 }
